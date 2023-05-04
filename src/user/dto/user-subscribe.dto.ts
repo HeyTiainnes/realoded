e944f91f4e2c0c5cb59c1141fcf191c6d5f1c579
HeyTiainnes <etienne.pomente@reseau.sncf.fr>
2023-05-04 09:23:38 +0200

    test user-task = OK

--- src/user/dto/user-subscribe.dto.ts
+++ src/user/dto/user-subscribe.dto.ts
@@ -11,4 +11,3 @@ export class UserSubscribeDto {
     @IsNotEmpty()
     password: string;
 }
-//
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -21,23 +21,16 @@ export class UserService {
         });
         user.salt = await bcrypt.genSalt();
         user.password = await bcrypt.hash(user.password, user.salt);
-        //this.userRepository.save(user);
-
         try {
             await this.userRepository.save(user);
         } catch (e) {
             if (e instanceof QueryFailedError) {
-                // Vous pouvez vérifier le code d'erreur pour vous assurer qu'il s'agit d'une violation de contrainte d'unicité
-                // et renvoyer un message d'erreur approprié
                 console.log('erreur :', e)
                 throw new ConflictException("L'identifiant, l'e-mail et le mot de passe doivent être uniques.");
             } else {
-                // Vous pouvez gérer d'autres erreurs ici ou les laisser se propager
                 throw e;
             }
         }
-        // delete user.salt;
-        // delete user.password;
         return {
             id: user.id,
             username: user.username,
@@ -46,19 +39,14 @@ export class UserService {
         };
     }
     async login(credentials: loginCredentialsDto) {
-
         const { username, password } = credentials
-
         const user = await this.userRepository.createQueryBuilder("user")
             .where("user.username = :username or user.email = :username",
                 { username }
             )
             .getOne()
         console.log(username, password);
-
-
         if (!user)
-
             throw new NotFoundException(`l\'utilisateur (ou le mail, ou le password) ${username}  n'existe(nt) pas dans la base de donnée`)
 
         const hashedPassword = await bcrypt.hash(password, user.salt);
@@ -72,29 +60,11 @@ export class UserService {
             return {
                 "acces_token": jwt
             };
-
-
-
             console.log('req:', Request);
             console.log('rep:', Response);
-            // return {
-
-            //     username: user.username,
-            //     email: user.email,
-            //     role: user.role
-            // }
-
         } else {
-
             throw new NotFoundException(`l\'utilisateur (ou le mail, ou le password) ${username}  n'existe(nt) pas dans la base de donnée`);
             console.log('comparaison donnees false')
         }
-
-
     }
-
-
-
-
-
 }
